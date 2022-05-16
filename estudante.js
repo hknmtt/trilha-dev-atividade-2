206afec88a8f388881d75ac22b00207666a9419f
Matheus S <matheussouza4555@gmail.com>
2022-05-15 21:34:30 -0300

    Imprimindo média do estudante

--- estudante.js
+++ estudante.js
@@ -5,3 +5,11 @@ var nota2 = 8.5;
 var nota3 = 6.9;
 
 mediaAluno = (nota1*4 + nota2*3 + nota3*3)/(4+3+3);
+
+console.log(`O nome do aluno é ${nomeAluno}, a idade do aluno é: ${idadeAluno}, e sua média é de ${mediaAluno}`);
+
+if (mediaAluno >= 7) {
+    console.log("Aluno aprovado");
+} else {
+    console.log("Aluno reprovado");
+}
